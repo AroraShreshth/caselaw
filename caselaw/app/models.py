ec24a2e5263b8f41b2cfc7230197bf597806ae64
Shreshth Arora <sarora_be18@thapar.edu>
2020-03-26 08:33:44 +0530

    migrations and errors in models.py files corrected

--- caselaw/app/models.py
+++ caselaw/app/models.py
@@ -35,13 +35,13 @@ class Case(models.Model):
     #Foreign KEy to User Model
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=300)
-    case_id = models.CharField(mx_length=50, null=True, blank=True)
+    case_id = models.CharField(max_length=50, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     date_posted = models.DateTimeField(default=timezone.now, editable=False)
     modified_date = models.DateTimeField(default=timezone.now)
     contribution_credit = models.NullBooleanField()
     consent = models.BooleanField()
-    country = models.ForeignKey(Country)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
     cc_license = models.ForeignKey(License, on_delete=models.PROTECT)
     jurisdiction = models.CharField(max_length=500, null=True, blank=True, help_text="Within Country")
     court_name = models.CharField(max_length=500, help_text="Original and/or English Translation")
@@ -63,7 +63,7 @@ class LinkCase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField()
     type = models.CharField(max_length=100, null=True, blank=True)
-    case = models.ForeignKey(Case)
+    case = models.ForeignKey(Case, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'{self.type} {self.url}'
@@ -75,7 +75,7 @@ class Scholarship(models.Model):
     modified_date = models.DateTimeField(default=timezone.now)
     contribution_credit = models.NullBooleanField()
     consent = models.BooleanField()
-    country = models.ForeignKey(Country)
+    country = models.ForeignKey(Country , on_delete=models.PROTECT)
     title = models.CharField(max_length=500)
     publisher = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -90,7 +90,7 @@ class LinkScholarship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField()
     type = models.CharField(max_length=100, null=True, blank=True)
-    case = models.ForeignKey(Scholarship)
+    case = models.ForeignKey(Scholarship, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'{self.type} {self.url}'
