09b43a05cf088ba2994b0c7372d5c1445b74c25f
Shreshth Arora <sarora_be18@thapar.edu>
2020-03-26 08:30:44 +0530

    settings and model changes

--- caselaw/app/models.py
+++ caselaw/app/models.py
@@ -10,21 +10,32 @@ class Categories(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.name}'
+
 #Models for Cases
 class Country(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class License(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Case(models.Model):
 
     #Foreign KEy to User Model
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=300)
+    case_id = models.CharField(mx_length=50, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     date_posted = models.DateTimeField(default=timezone.now, editable=False)
     modified_date = models.DateTimeField(default=timezone.now)
@@ -32,23 +43,30 @@ class Case(models.Model):
     consent = models.BooleanField()
     country = models.ForeignKey(Country)
     cc_license = models.ForeignKey(License, on_delete=models.PROTECT)
-    jurisdiction = models.CharField(max_length=500, null=True, help_text="Within Country")
+    jurisdiction = models.CharField(max_length=500, null=True, blank=True, help_text="Within Country")
     court_name = models.CharField(max_length=500, help_text="Original and/or English Translation")
-    decision_place = models.CharField(max_length=500, null=True)
-    decision_date = models.DateTimeField(null=True) 
+    decision_place = models.CharField(max_length=500, null=True, blank=True)
+    decision_date = models.DateTimeField(null=True, blank=True) 
     link_decesion1 = models.URLField()
-    link_decesion_eng_translated = models.URLField(null=True)
-    background_info = models.TextField(max_length=5000, null=True)
-    summary_decesion = models.TextField(max_length=5000, null=True)
-    result_ruling = models.TextField(max_length=5000, null=True)
-    implication = models.TextField(max_length=5000, null=True)
+    link_decesion_eng_translated = models.URLField(null=True, blank=True)
+    background_info = models.TextField(max_length=5000, null=True, blank=True)
+    summary_decesion = models.TextField(max_length=5000, null=True, blank=True)
+    result_ruling = models.TextField(max_length=5000, null=True, blank=True)
+    implication = models.TextField(max_length=5000, null=True, blank=True)
     categories = models.ManyToManyField(Categories)
     published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
 class LinkCase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField()
-    type = models.CharField(max_length=100, ${blank=True, null=True})
+    type = models.CharField(max_length=100, null=True, blank=True)
     case = models.ForeignKey(Case)
+
+    def __str__(self):
+        return f'{self.type} {self.url}'
 
 #Scholarship Data Models
 class Scholarship(models.Model):
@@ -65,9 +83,15 @@ class Scholarship(models.Model):
     abstract = models.TextField(max_length=5000)
     category = models.ManyToManyField(Categories)
 
+    def __str__(self):
+        return f'{self.title}'
+
 class LinkScholarship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField()
-    type = models.CharField(max_length=100, ${blank=True, null=True})
+    type = models.CharField(max_length=100, null=True, blank=True)
     case = models.ForeignKey(Scholarship)
+
+    def __str__(self):
+        return f'{self.type} {self.url}'
     
